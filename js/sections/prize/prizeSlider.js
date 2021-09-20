1a7357d5d60dcc00046e41adc42cb39b432d6df1
Yaroslav-Bondar <jamnetbox@gmail.com>
2021-09-20 22:05:54 +0300

    modified invocation of instances of the Overlap object

--- js/sections/prize/prizeSlider.js
+++ js/sections/prize/prizeSlider.js
@@ -1,8 +1,20 @@
 let prizeSlider = document.querySelector('.prize__slider')
 let prizeSlides = document.getElementsByClassName('prize__slide')
 let prizeDots = document.getElementsByClassName('prize__dot')
+let prizeSlideDescrips = document.querySelectorAll('.prize__slide-descrip')
+let prizeDescribe = document.querySelector('.prize__describe')
+let prizeTitle = document.querySelector('.prize__title')
 
 let slideIndex = 1
+let overlapA
+let overlapB
+let isOverlap = false
+
+function setIsOverlap(val) {
+  if(!isOverlap) {
+    isOverlap = val
+  }
+}
 
 function showSlides (n) {
   // spin slides in a circle
@@ -16,8 +28,31 @@ function showSlides (n) {
   }
   prizeSlides[slideIndex - 1].style.display = 'block'
   prizeDots[slideIndex - 1].classList.add('prize__dot_active')
+
+  // eliminate the intersection with the current slide
+  overlapA = new Overlap(prizeDescribe, prizeSlideDescrips[slideIndex - 1], 50)
+  overlapA.changeOverlap()
+  setIsOverlap(overlapA.isOverlap)
+  // eliminate the intersection with the current slide
+  overlapB = new Overlap(prizeTitle, prizeSlideDescrips[slideIndex - 1], 50)
+  overlapB.changeOverlap()
+  setIsOverlap(overlapB.isOverlap)
 }
 showSlides(slideIndex)
+
+// we eliminate the intersection 
+// and adjust the indentation with 
+// the current slide when resizing the browser window
+window.addEventListener('resize', ()=> {
+    overlapA.changeOverlap()
+    overlapB.changeOverlap()
+    if(isOverlap) {
+        overlapA.killGap()
+    }
+    if(isOverlap) {
+        overlapB.killGap()
+    }
+})
 
 // click on slider conteiner
 prizeSlider.addEventListener('click', (e)=> {
