4d71e504660d7e777b9f67a65b03d4493cae5983
Yaroslav-Bondar <jamnetbox@gmail.com>
2022-01-31 22:04:16 +0200

    modals for buttons

---
+++ js/modal/modal.js
@@ -0,0 +1,14 @@
+
+// close modal window
+document.body.addEventListener('click', (e) => {
+    if(!e.target.classList.contains('modal__close-btn') && !e.target.classList.contains('modal')) return;
+    e.target.closest('.modal').style.display = 'none';
+    body.style.overflow = '';
+});
+// show modal window
+document.body.addEventListener('click', (e) => {
+    if(!e.target.dataset.modal) return;
+    document.querySelector(`#${e.target.dataset.modal}`).style.display = 'flex';
+    body.style.overflow = 'hidden';
+});
+
--- js/modal/modalClose.js
+++
@@ -1,4 +0,0 @@
-document.body.addEventListener('click', (e) => {
-    if(!e.target.classList.contains('modal__close-btn')) return;
-    e.target.closest('.modal').style.display = 'none';
-});
